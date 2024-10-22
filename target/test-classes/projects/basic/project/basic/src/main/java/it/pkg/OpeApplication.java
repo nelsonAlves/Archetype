cdf0fe0e630a55de5308d86bdb47227f748e3463
nelson alves de oliveira <nelsonalves.olv@gmail.com>
2024-10-21 23:19:57 -0300

    Criação de archetypes

---
+++ target/test-classes/projects/basic/project/basic/src/main/java/it/pkg/OpeApplication.java
@@ -0,0 +1,13 @@
+package it.pkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpeApplication.class, args);
+	}
+
+}
